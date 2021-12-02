50efd00b0838025415a4649c67c807284ae9683b
Mario Toruño <80336294+MarioLanza25@users.noreply.github.com>
2021-12-01 20:24:27 -0600

    Add files via upload

---
+++ Practica-wpf/Practicas/DataGridView/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridView
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Telefono> telefonos = new List<Telefono>();
+
+            telefonos.Add(new Telefono("S21 plus", "Samsung", 720.2f));
+            telefonos.Add(new Telefono("S21 ", "Samsung", 650));
+            telefonos.Add(new Telefono("P40 pro", "Huawei", 620));
+            telefonos.Add(new Telefono("Mi 11 pro", "Xiaomi", 700.5f));
+
+            dgvProductos.ItemsSource = telefonos;
+
+
+        }
+
+        private void btnSeleccionar_Click(object sender, RoutedEventArgs e)
+        {
+            Telefono telefono = (Telefono)dgvProductos.SelectedItem;
+
+            string msj = string.Format("El modelo {0} de la marca {1} tiene como precio ${2}", 
+                telefono.Nombre, telefono.Marca, telefono.Precio);
+
+            MessageBox.Show(msj, "Producto seleccionado");
+        }
+    }
+}
---
+++ Practica-wpf/Practicas/DataGridView/Telefono.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridView
+{
+    class Telefono
+    {
+        private string nombre;
+        private string marca;
+        private double precio;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public double Precio { get => precio; set => precio = value; }
+
+        public Telefono(string nombre, string marca, double precio)
+        {
+            this.nombre = nombre;
+            this.marca = marca;
+            this.precio = precio;
+        }
+    }
+}
