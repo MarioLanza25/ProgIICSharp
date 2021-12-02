c2c8d5175747a7d4b66401f7be6dac19e7c06cf7
Mario Toruño <80336294+MarioLanza25@users.noreply.github.com>
2021-12-01 20:21:20 -0600

    Add files via upload

---
+++ Practica-wpf/Practicas/ListBox/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Departamentos> depts = new List<Departamentos>();
+
+            depts.Add(new Departamentos(){Departamento1 ="Managua", Temperatura1 = 29, Departamento2= "Leon", Temperatura2 = 31});
+            depts.Add(new Departamentos() { Departamento1 = "Chinandega", Temperatura1 = 32, Departamento2 = "Masaya", Temperatura2 = 29 });
+            depts.Add(new Departamentos() { Departamento1 = "Matagalpa", Temperatura1 = 29, Departamento2 = "Estelí", Temperatura2 = 27 });
+            depts.Add(new Departamentos() { Departamento1 = "Jinotega", Temperatura1 = 29, Departamento2 = "Granada", Temperatura2 = 30 });
+            depts.Add(new Departamentos() { Departamento1 = "Carazo", Temperatura1 = 27, Departamento2 = "Rivas", Temperatura2 = 28 });
+
+            listaDepts.ItemsSource = depts;
+
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            if (listaDepts.SelectedItem != null)
+            {
+                MessageBox.Show((listaDepts.SelectedItem as Departamentos).Departamento1 = " " +
+                    (listaDepts.SelectedItem as Departamentos).Temperatura1 + "C " +
+                (listaDepts.SelectedItem as Departamentos).Departamento2 + "C " +
+                (listaDepts.SelectedItem as Departamentos).Temperatura2 + "C ");
+            }
+            else
+                MessageBox.Show("Seleccione un departamento");
+        }
+
+        public class Departamentos
+        {
+            public string Departamento1 { get; set; }
+
+            public int Temperatura1 { get; set; }
+
+            public string Departamento2 { get; set; }
+
+            public int Temperatura2 { get; set; }
+
+            public int DiferenciaTmp {
+                get {
+                    if (Temperatura2 > Temperatura1)
+                        return Temperatura2 - Temperatura1;
+                    else
+                        return Temperatura1 - Temperatura2;
+                }
+                set { }
+            }
+        }
+    }
+}
